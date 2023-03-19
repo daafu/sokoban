04e7f829794f10ec36a3bb6f4a5665d09094fd80
Martin Cietwierkowski <mcietwie@gmail.com>
2023-03-19 17:10:32 -0400

    Update release scripts

--- release.bat
+++ release.bat
@@ -1,1 +1,11 @@
 jai src/build.jai -release -exe piotr -output_path ..
+
+if exist "deploy\windows" rmdir /s /q deploy\windows
+
+if not exist "deploy" mkdir deploy
+if not exist "deploy\windows" mkdir deploy\windows
+if not exist "deploy\windows\res" mkdir deploy\windows
+
+xcopy piotr.exe deploy\windows\ /Y
+xcopy SDL2.dll deploy\windows\ /Y
+xcopy res deploy\windows\res\ /E /Y
--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 jai-linux src/build.jai -release -exe piotr -output_path ..
 
+rm -rf deploy/linux
 mkdir -p deploy/linux
 cp  piotr deploy/linux
 cp -r res deploy/linux
