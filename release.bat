33a35ad47d39fe24bb529b4e916ec37c20eb5f02
Mehmet Karadeniz <mehmetkaradeniz@users.noreply.github.com>
2023-11-18 08:12:32 +0300

    fix mkdir patch

--- release.bat
+++ release.bat
@@ -4,7 +4,7 @@ if exist "deploy\windows" rmdir /s /q deploy\windows
 
 if not exist "deploy" mkdir deploy
 if not exist "deploy\windows" mkdir deploy\windows
-if not exist "deploy\windows\res" mkdir deploy\windows
+if not exist "deploy\windows\res" mkdir deploy\windows\res
 
 xcopy piotr.exe deploy\windows\ /Y
 xcopy SDL2.dll deploy\windows\ /Y
